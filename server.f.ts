a5864a4c5daf03d6ee90cf6338e714445d95bbe9
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-03-12 07:41:26 -0700

    simple file server

---
+++ server.f.ts
@@ -0,0 +1,63 @@
+import { utf8 } from 'functionalscript/text/module.f.js'
+import { type Vec } from 'functionalscript/types/bit_vec/module.f.js'
+import { pure } from 'functionalscript/types/effects/module.f.js'
+import {
+    createServer,
+    listen,
+    forever,
+    type NodeProgram,
+    type IncomingMessage,
+    readFile,
+    type ServerResponse,
+    log,
+    readdir,
+    type IoResult,
+    type Dirent,
+} from 'functionalscript/types/effects/node/module.f.js'
+import { htmlToString } from 'functionalscript/html/module.f.js'
+import { concat } from 'functionalscript/path/module.f.js'
+
+const listener = ({ url }: IncomingMessage) => {
+    const path = url.split('?')[0] ?? ''
+    const file = '.' + path
+
+    const dirLink = ({ name, isFile }: Dirent) => {
+        const href = '/' + concat(path)(name)
+        return [['a', { href }, name + (isFile ? '' : '/')], '\n'] as const
+    }
+
+    const dirPage = (v: readonly Dirent[]) => htmlToString(
+        ['html',
+            ['head',
+                ['link', { rel: 'stylesheet', href: '/main.css' }]
+            ],
+            ['body',
+                ['pre',
+                    ...v.flatMap(dirLink)
+                ]
+            ]
+        ])
+
+    const orReadDir = (r: IoResult<Vec>) => r[0] === 'ok'
+        ? pure(r)
+        : readdir(file, {})
+            .step(([s, v]) => pure([s, utf8(s === 'ok' ? dirPage(v) : '')] as const))
+
+    return log(`reading ${file}`)
+        .step(() => readFile(file))
+        .step(orReadDir)
+        .step(([s, v]) => {
+            const o = s === 'ok'
+            return pure({
+                status: o ? 200 : 404,
+                headers: {},
+                body: o ? v : utf8('404 not found'),
+            })
+        })
+}
+
+const main: NodeProgram = () => createServer(listener)
+    .step(server => listen(server, 3000))
+    .step(forever)
+
+export default main
